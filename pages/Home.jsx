1fdbef739d04bf944cddb3903c4d690b9f7703ee
Lbgraff <lbgraff92@gmail.com>
2024-09-17 17:00:13 -0500

    updated personal and professional info sections on home page

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -6,15 +6,12 @@ export default function Home() {
                 <h2 class="header 2">Professional Info</h2>
                 <p>I graduated from the University of Texas with a degree in anthropology and a minor in French. After graduation, I
                     was offered a job at Apple translating French for Apple Maps. This was my introduction to the tech world, which
-                    prompted me to learn coding. After exploring several other career options, including teaching middle school
-                    special ed and working as a professional archaeologist, I have found my way back into coding as something I am
-                    interested in as a longterm career. I am very excited about being a student in the UT Coding Boot Camp and enjoy
-                    learning more coding skills!
+                    prompted me to learn coding. I taught middle school special ed for two years during the pandemic. I worked as a professional archaeologist, which was my childhood dream. I have found my way back into coding as something I am interested in as a longterm career. I am very excited about coding and have developed a newfound love of computer science. I enjoy learning new coding languages and skills and can't wait to start my career as a programmer.
                 </p>
             </section>
             <section id="personal-info">
                 <h2 class="header 2">Personal Info</h2>
-                <p>I grew up in Omaha, NE until I moved with my family to Austin, TX when I was 13. I have four sisters and no brothers. I enjoy singing, journaling, and spending time with my family and in nature.
+                <p>I grew up in Omaha, NE until I moved with my family to Austin, TX when I was 13. I have four sisters and no brothers. I enjoy singing, journaling, and spending time with my family and in nature. I speak French semi-fluently and have dabbled in Spanish, German, and Italian. I love learning spoken languages as much as I love learning coding ones!
                 </p>
             </section>
         </div>
