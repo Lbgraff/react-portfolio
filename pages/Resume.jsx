c8bcb14cb33f4ac9152a0389a94a40adce2ed1e8
Lbgraff <lbgraff92@gmail.com>
2024-09-17 16:51:12 -0500

    added changes/updates to resume to match current resume

--- pages/Resume.jsx
+++ pages/Resume.jsx
@@ -4,69 +4,78 @@ export default function Resume() {
             <div style={{ whiteSpace: 'pre-line' }}>
                 <h1>Leigh B. Graff</h1>
                 <p>
-                Austin, TX{"\n"}
+                    Austin, TX{"\n"}
                     Phone: 512-937-7168 | Email: Lbgraff92@gmail.com{"\n"}
                     LinkedIn: https://www.linkedin.com/in/leigh-graff-87135a47/
                     GitHub: https://github.com/Lbgraff
-                    Portfolio: https://lbgraff.github.io/Portfolio/ 
-                    </p>{"\n"}
+                    Portfolio: https://lbgraff.github.io/Portfolio/
+                </p>{"\n"}
 
                 <h2>Summary</h2>
-                <p>Full Stack Web Developer with a background in Anthropology and Foreign Languages. Adept at both creative and problem solving skills. Known for strong attention to detail and creative, out-of-the-box solutions. Former special education teacher and archaeologist. Value empathy, curiosity, tenacity, and positivity.</p>
+                <p>Full Stack Web Developer with a background in anthropology and foreign languages. Innovative problem-solver known for strong attention to detail and out-of-the-box solutions. Former special education teacher and archaeologist who values positivity, empathy, curiosity, and tenacity. Passionate about creating user-friendly, intuitive, and secure website from scratch with interactive features to optimize traffic and user experience.
+                </p>
                 {"\n"}
                 <h2>Technical Skills</h2>
                 <p><strong>Technical Languages: </strong>
-                      HTML, Git, CSS, JavaScript, NodeJS, Express.js, SQL, NoSQL, React, MERN{"\n"}
-                    <strong>Applications:</strong> APIs, Object-Oriented Programming, Object-Relational Mapping, Model-View-Controller, Progressive Web Applications, State</p>{"\n"}
+                    HTML5, Git, CSS3, JavaScript ES6+, jQuery, Node, Express, SQL, NoSQL, Handlebars, React, GraphQL, MongoDB, MERN{"\n"}
+                    <strong>Skills: </strong> APIs, Object-Oriented Programming, Object-Relational Mapping, Model-View-Controller, Progressive Web Applications, State, Computer Science, Documentation, Code Optimization, Mobile-First Design</p>{"\n"}
 
                 <h2>Projects</h2>
-                <p><strong>Pantry Rescue | https://github.com/olivercappis/pantry-rescue | https://olivercappis.github.io/pantry-rescue/
-                    Front end application using input to make API call for a list of recipes.
-                    Responsible for JavaScript.
-                    HTML, Git, CSS, JavaScript{"\n"}
+                <p><strong>Pantry Rescue | <a href='https://github.com/olivercappis/pantry-rescue'>Link to Github</a> | <a href='https://olivercappis.github.io/pantry-rescue/'>Deployed Link</a>
+                    Users input ingredients to make a call to a library of recipes.{"\n"}
+                    Front-end developer{"\n"}
+                    HTML, JavaScript{"\n"}
 
-                    Love Connect | https://github.com/olivercappis/project-2 | https://loveconnect.onrender.com
-                    Front and back-end application that stores data in multiple connecting databases.
-                    Responsible for Bootstrap, JavaScript, Handlebars Views
-                    HTML, CSS, JavaScript, SQL, Handlebars</strong></p>{"\n"}
-                    {"\n"}
+                    Love Connect | <a href='https://github.com/olivercappis/project-2'>Link to Github</a> | <a href='https://loveconnect.onrender.com'>Deployed Link</a>
+                    Front and back-end dating site application that stores data in multiple connecting databases.{"\n"}
+                    Front-end developer{"\n"}
+                    HTML, JavaScript, Handlebars{"\n"}
+
+                    Exit-Survey App | <a href='https://github.com/wzieb/Exit-Survey'>Link to Github</a> | <a href='https://loveconnect.onrender.com'>Deployed Link</a>
+                    Front and back-end application for managing end-of-course surveys.{"\n"}
+                    Front end developer{"\n"}
+                    JavaScript, React{"\n"}
+
+                </strong></p>{"\n"}
+                {"\n"}
                 <h2>Relevant Experience</h2>
                 <p><strong>Project Manager</strong>{"\n"}
                     2022 – 2023{"\n"}
                     <strong>ProMiles Software Development Corporation</strong>{"\n"}
                     Austin, TX{"\n"}
-                    Zoom video meetings with state departments of transportation to assess their needs for the software.
-                    Wrote and edited technical documents.
-                    Daily collaboration and teamwork with project management team and software developers.
+                    Communication with state departments of transportation (clients) to assess their needs for the software.
+                    Technical writing and editing.{"\n"}
+                    Daily collaboration and teamwork with project management team and software developers.{"\n"}
                     Tested software for quality assurance and communicated with developers to solve issues.{"\n"}
                     {"\n"}
                     <strong>Bilingual Transit Analyst - French language</strong>{"\n"}
-                    2017 – 2019{"\n"}
+                    2017 - 2018, 2018 - 2019 (contract){"\n"}
                     <strong>Apple Inc via Apex Systems</strong>{"\n"}
                     Austin, TX{"\n"}
                     Data analysis that required attention to detail to edit maps for Apple Maps.{"\n"}
                     Researched and wrote content in French and English that went live to the client app immediately.{"\n"}
-                    Collaborated with team members.{"\n"}
+                    Collaboration with team members.{"\n"}
                     Nominated to participate in five extra projects, including handling escalations from entry-level projects and sitting on a project advisory board.{"\n"}
                     {"\n"}
                     <strong>Copywriter and Editor</strong>{"\n"}
                     2016 – 2016{"\n"}
                     <strong>Regard Jewelry</strong>{"\n"}
                     Austin, TX{"\n"}
-                    Communicated and collaborated with the owner of the company and other team members to write biographies that were published on the company website.{"\n"}
-                    Researched the jewelry industry and its history to write copy for the website.{"\n"}
+                    Communicated and collaborated with the owner of the company and other team members.{"\n"}
+                    Researched the jewelry industry and its history to write web content.
                     {"\n"}
                     <strong>Copywriter and Editor</strong>{"\n"}
                     2016 – 2016{"\n"}
                     <strong>AquaSprouts, LLC</strong>{"\n"}
                     Austin, TX{"\n"}
-                    Collaborated with the owner of the company to learn company objectives and product information in order to write web content.</p>{"\n"}
+                    Collaborated with the owner of the company.{"\n"}
+                    Learned company objectives and product information to write web content.</p>{"\n"}
 
                 <h2>Education</h2>
                 <p><strong>Full Stack Web Development Boot Camp Certificate:</strong> University of Texas at Austin, Austin, TX{"\n"}
                     An intensive program focused on gaining technical programming skills.{"\n"}
                     <strong>Bachelor’s Degree:</strong> University of Texas at Austin, Austin, TX </p>
-                    </div>
-</div>
+            </div>
+        </div>
     );
 }
